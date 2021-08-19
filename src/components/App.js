d9577d877a6682ac31e8d22451ecbf5f71ffe28e
nikhiltadikonda <nikhiltadikonda@gmail.com>
2021-08-19 21:57:26 +0530

    File and Directory Modifications

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>This site is under development.👷🏗</h1>
+    </div>
+  );
+}
+
+export default App;
