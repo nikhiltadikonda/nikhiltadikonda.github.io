ebefdaa4d3ceeef5fa76df85f9f6dd539df34e15
nikhiltadikonda <nikhiltadikonda@gmail.com>
2021-09-18 06:44:16 +0530

    Added Socialify Images

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -3,17 +3,17 @@ import {Button, Col, Card} from 'react-bootstrap';
 
 function ProjectCard(props){
 
+    var url = `${process.env.REACT_APP_SOCIALIFY_URL}/${props.owner.login}/${props.name}/${process.env.REACT_APP_SOCIALIFY_URL_PARAM}`;
+
     return(
         <Col key = {props.id} className="project-column" lg={4} md={6}>
-            <Card className = "project-card repo-card"> 
-                <Card.Title>
-                    <h3 className="project-box-title">{props.name}</h3>
-                </Card.Title>
-                <hr/>   
+            <Card className = "project-card repo-card">
+                <Card.Img variant="top" src={url} /> 
                 <Card.Body>
-                    <p>
-                        {props.description == null ? "No description available" : (props.description.length > 100 ? props.description.substring(0, 100) + "..." : props.description)}
-                    </p>
+                    <Card.Title>
+                        <h3 className="project-box-title">{props.name}</h3>
+                    </Card.Title>
+                    <br />
                     <Button variant = "primary">Open on Github</Button>
                 </Card.Body>
             </Card>
